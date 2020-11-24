136c6758546ca7b542281c606db32ccd8271ef09
zhangkai7 <kaiz1121@163.com>
2020-11-24 13:36:53 +0800

    feat:[28]-实现strStr()-[简单]

---
+++ src/leetcode/editor/cn/ImplementStrstr.java
@@ -0,0 +1,59 @@
+//实现 strStr() 函数。
+//
+// 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如
+//果不存在，则返回 -1。
+//
+// 示例 1:
+//
+// 输入: haystack = "hello", needle = "ll"
+//输出: 2
+//
+//
+// 示例 2:
+//
+// 输入: haystack = "aaaaa", needle = "bba"
+//输出: -1
+//
+//
+// 说明:
+//
+// 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+//
+// 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与C语言的 strstr() 以及 Java的 indexOf() 定义相符。
+// Related Topics 双指针 字符串
+// 👍 624 👎 0
+
+package leetcode.editor.cn;
+public class ImplementStrstr {
+    public static void main(String[] args) {
+        Solution solution = new ImplementStrstr().new Solution();
+        String haystack = "hello", needle = "ll";
+        String haystack2 = "abc", needle2 = "c";
+        System.out.println(solution.strStr(haystack, needle));
+        System.out.println(solution.strStr(haystack2, needle2));
+        System.out.println(solution.strStr("", "11"));
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //模式匹配
+    public int strStr(String haystack, String needle) {
+        int i=0,j=0;
+        while((i<haystack.length()) && (j<needle.length())) {
+            if(haystack.charAt(i)==needle.charAt(j)) {
+                i++;
+                j++;
+            }else {
+                i=i-j+1;
+                j=0;
+            }
+        }
+        if(j==needle.length())
+            return i-needle.length();
+        else
+            return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
